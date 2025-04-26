d8a333c33ee2034df0e6555432630cebfecdb33c
Franklin-033 <94736900+Franklin-033@users.noreply.github.com>
2025-04-26 14:06:30 +0530

    Update test_main.py

--- Test/test_main.py
+++ Test/test_main.py
@@ -1,4 +1,10 @@
-from App.main  import add
+import unittest
+from App.main import add
 
-def test_add():
-    assert add(2,3)==5
+class TestAddFunction(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+#if name == '__main__':
+if __name__ == '__main__':  
+    unittest.main()
